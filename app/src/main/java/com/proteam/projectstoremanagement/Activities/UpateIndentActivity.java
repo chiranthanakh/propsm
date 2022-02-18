bbaf338ea794bc8fdcd16d835e794955a294c1bb
ProTeamit <97509135+ProTeamit@users.noreply.github.com>
2022-02-18 15:51:49 +0530

    sublocation

--- app/src/main/java/com/proteam/projectstoremanagement/Activities/UpateIndentActivity.java
+++ app/src/main/java/com/proteam/projectstoremanagement/Activities/UpateIndentActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.ListView;
 
+import com.proteam.projectstoremanagement.Adapters.IndentStatusAdapter;
+import com.proteam.projectstoremanagement.Adapters.UpdateIndentAdapter;
+import com.proteam.projectstoremanagement.Model.IndentStatusModel;
+import com.proteam.projectstoremanagement.Model.UpdateIndentModel;
 import com.proteam.projectstoremanagement.R;
+
+import java.util.ArrayList;
 
 public class UpateIndentActivity extends AppCompatActivity {
     ImageView mToolbar;
+    ListView lv_update_indent_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +24,25 @@ public class UpateIndentActivity extends AppCompatActivity {
         mToolbar = findViewById(R.id.back_toolbar);
         mToolbar.setOnClickListener(view -> onBackPressed());
 
+        lv_update_indent_list=findViewById(R.id.lv_update_indent_list);
+
+
+
+        final ArrayList<UpdateIndentModel> arrayList = new ArrayList<UpdateIndentModel>();
+
+        arrayList.add(new UpdateIndentModel("1","Mondal Enterprises","454","6777"));
+        arrayList.add(new UpdateIndentModel("2","Bright Energy","4545","5555"));
+        arrayList.add(new UpdateIndentModel("3","Shree Krishna Precast","45455","59898"));
+
+
+        // Now create the instance of the NumebrsViewAdapter and pass
+        // the context and arrayList created above
+        UpdateIndentAdapter numbersArrayAdapter = new UpdateIndentAdapter(this, arrayList);
+
+        // create the instance of the ListView to set the numbersViewAdapter
+        ListView updateindentList = findViewById(R.id.lv_update_indent_list);
+
+        // set the numbersViewAdapter for ListView
+        updateindentList.setAdapter(numbersArrayAdapter);
     }
 }
